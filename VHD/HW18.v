b02042abb27df01e78cf3bd086264479c3798793
unknown <brian123483@yahoo.com.tw>
2023-05-18 11:49:39 +0800

    HW18OK

--- VHD/HW18.v
+++ VHD/HW18.v
@@ -5,25 +5,25 @@ module HW18
 	output reg [7:0]BCD,BCD1,BCD2,BCD3
 );
 
-	reg [3:0]num[0:9];
+	reg [7:0]num[0:9];
 	reg [9:0]w;
 	initial begin
-           num[0] =8'h60;
-           num[1] =8'h6D;
-           num[2] =8'h79;
-           num[3] =8'h33;
-           num[4] =8'h5B;
-           num[5] =8'h1F;
-           num[6] =8'h70;
-           num[7] =8'h7F;
-           num[8] =8'h73;
-           num[9] =8'h77;
-		   w = 10'd70;
+           num[0] =8'h03;
+           num[1] =8'h9F;
+           num[2] =8'h25;
+           num[3] =8'h0D;
+           num[4] =8'h99;
+           num[5] =8'h49;
+           num[6] =8'hC1;
+           num[7] =8'h1F;
+           num[8] =8'h01;
+           num[9] =8'h19;
+		   w = 10'd5;
    end
 
 	reg [30:0] q;
 	reg [1:0]btnr;
-	reg [7:0]x; 
+	integer x; 
 	reg [9:0]t;
 	reg flag,flag1;
 	reg [3:0]a,b;
@@ -33,42 +33,48 @@ module HW18
 	always @ (posedge CLK)
 	begin
 		q <= q + 1;
-		BCD1	= num[a];
-		BCD	= num[b];
+		BCD1	<= num[a];
+		BCD	<= num[b];
+		BCD2	<= num[3];
+		BCD3	<= num[4];
+		//flag <= flag1;
 	end
 
-	always @ (posedge q[20])
-	begin
-		if(btn != 4'b1111)begin
-			case(btn)
-				4'b0001:	btnr <= 2'd1;
-				4'b0010:	btnr <= 2'd2;
-				4'b0100:	btnr <= 2'd0;
-			endcase;
-			if(t <= 10'd100)
-				t <= t + 1;
-			else begin
-				flag <= 1'b0;t <= 0;end
-				
-		a <= x / 10;
-		b <= x % 10;
-		end
-	end
 	
-	always @ (posedge q[25])
+	
+	always @ (posedge q[23])
 	begin
-		if(t < w && flag == 1'b0)begin
-			case(btnr)
-				2'd1:	x <= x + 1;
-				2'd2:	x <= x - 1;
-			endcase;
+	
+	a <= (x / 10)%10;
+	b <= x % 10;
+	
+	if(btn != 4'b1111)
+		begin
+			if(t < w)
+				begin
+					if(flag == 1'b0)
+						begin
+							case(btn)
+								4'b1110:	x <= x + 1;
+								4'b1101:	x <= x - 1;
+							endcase;
+							flag <= 1;
+						end
+					t <= t + 1;
+				end
+			else if(t == w)
+				begin
+					case(btn)
+						4'b1110:	x <= x + 1;
+						4'b1101:	x <= x - 1;
+						4'b1011:	x <= 0;
+					endcase;
+				end
 		end
-		else if(t >= w)begin
-			case(btnr)
-				2'd0:	x <= 0;
-				2'd1:	x <= x + 1;
-				2'd2:	x <= x - 1;
-			endcase;
+	else if(btn == 4'b1111)
+		begin 
+			t <= 0;
+			flag <= 0;
 		end
 	end
 	
